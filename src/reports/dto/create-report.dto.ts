a292beeae934eef7361fd620bf97c118bdb7b283
ryanvitor007 <ryanv7876@gmail.com>
2026-01-23 16:24:30 -0300

    publicação da branch nova

--- src/reports/dto/create-report.dto.ts
+++ src/reports/dto/create-report.dto.ts
@@ -2,7 +2,7 @@ export class CreateReportDto {
   title: string;
   startDate: string;
   endDate: string;
-  vehicleIds: string; // Virá como string JSON ou separada por vírgula no FormData
+  vehicleIds: string;
   createdBy: string;
   type: string;
 }
